96eacd2af74246b8ccf1744fdeb091d65d4f24b9
varat21 <varat.thapa21@gmail.com>
2025-10-10 13:36:10 +0545

    ghsjnx

--- api/inngest/route.js
+++ api/inngest/route.js
@@ -5,7 +5,7 @@ import {
   syncUserUpdation,
   synUserDeletion,
 } from "@/config/inngest";
-
+//
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [synUserDeletion, syncUserUpdation, syncUserCreation],
